0b4b6ffd7ca59c2e0fd71707efe28ad0ffdc8236
cow-coding <kbp0237@gmail.com>
2022-05-25 08:30:32 +0000

    producer user code

---
+++ producer/producer_user.py
@@ -0,0 +1,122 @@
+import json
+import os
+import re
+import time
+from datetime import datetime
+
+import yaml
+import argparse
+from distutils.util import strtobool
+
+import requests
+from dotenv import load_dotenv
+
+from pymongo import MongoClient
+
+from redisqueue import RedisQueue
+
+from concurrent.futures import ThreadPoolExecutor
+from functools import partial
+
+
+def db_connect(user, passwd, database, **kwargs):
+    db_connection_str = f"mongodb+srv://{user}:{passwd}@cluster0.dojne.mongodb.net/{database}"
+
+    conn = MongoClient(db_connection_str)
+
+    return conn["final_project"]
+
+
+def get_repo_user_list(url, headers):
+    res = requests.get(url, headers=headers)
+    res.raise_for_status()
+
+    data = res.json()
+
+    return data
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    load_dotenv(verbose=True)
+
+    parser.add_argument("--token_idx", type=int, default=0)
+
+    args = parser.parse_args()
+
+    with open(os.getenv("SETTINGS")) as f:
+        settings = yaml.load(f, Loader=yaml.FullLoader)
+
+    token_list = settings["token"]
+    token_idx = args.token_idx
+    token = token_list[token_idx]
+    token_count = 1
+    data_profile = settings["data_profile"]
+    db_profile = settings["db_profile"]
+    cloud_info = settings["cloud_info"]
+    headers = {"Authorization": "token " + token,
+               "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) "
+                             "Chrome/84.0.4147.89 Safari/537.36"}
+
+    q = RedisQueue("final_project", host=cloud_info["host"], port=6379, db=0, decode_responses=True)
+
+    print("Get repository data")
+    db = db_connect(**db_profile)
+    collection = db.get_collection("repository")
+    repo_list = collection.find()
+
+    url_list = []
+
+    for repo in repo_list:
+        login = repo["login"]
+        repo_name = repo["repo_name"]
+        star_pages = 10 if repo["star_pages"] > 10 else repo["star_pages"]
+
+        base_url = f"https://api.github.com/repos/{login}/{repo_name}/stargazers?per_page=100&page="
+        tmp_url = [base_url + str(page) for page in range(1, star_pages + 1)]
+
+        url_list.extend(tmp_url)
+
+    url_idx = 0
+    uid_list = []
+    user_dict = dict()
+
+    while url_idx < len(url_list):
+        url = url_list[url_idx]
+
+        try:
+            data = get_repo_user_list(url, headers)
+
+            user_dict["uid"] = data["uid"]
+            user_dict["login"] = data["login"]
+
+            if data["uid"] not in uid_list:
+                insert_data = json.dumps(user_dict)
+                uid_list.append(data["uid"])
+                q.put(insert_data)
+
+            url_idx += 1
+        except requests.exceptions.HTTPError as httperr:
+            if httperr.response.status_code == 403:
+                if token_count >= len(token_list):
+                    print("Wait token reset")
+                    time.sleep(60 * 60)
+                    token_count = 1
+                else:
+                    token_count += 1
+                token_idx += 1
+                token_idx = token_idx % len(token_list)
+                token = token_list[token_idx]
+                headers["Authorization"] = "token "+token
+            elif httperr.response.status_code == 404:
+                url_idx += 1
+
+
+
+
+
+
+
+
+
